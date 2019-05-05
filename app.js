480bff5378438addcd94e84a80407dff21c67a58
Dane Williams <dwilli36@nd.edu>
2019-05-05 18:25:30 -0400

    added instagram and countdown

--- app.js
+++ app.js
@@ -87,3 +87,36 @@ app.controller('AlumniCtrl', ['$scope', function($scope){
 app.controller('WelcomeWeekendCtrl', ['$scope', function($scope){
     $scope.message = '';
 }]);
+
+
+//Countdown
+var countDownDate = new Date("Apr 25, 2020 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the results
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);

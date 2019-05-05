4a774a125da6ca3d3cf40747db9fdf193d21e3f4
Dane Williams <dwilli36@nd.edu>
2019-05-05 12:10:18 -0400

    added angular SPA functionality

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var app = angular.module('myApp', ['ngRoute']);
+'use strict';
+var app = angular.module("myApp", ['ngRoute']);
 
-app.config(function($routeProvider) {
-  $routeProvider
 
-  .when('/', {
-    templateUrl : 'index.html',
-    controller  : 'IndexController'
-  })
-
-  .when('/About', {
-    templateUrl : 'About.html',
-    controller  : 'AboutController'
-  })
-
-  .otherwise({redirectTo: '/'});
-});
-
-app.controller('IndexController', function($scope) {
-    $scope.message = 'Hello from HomeController';
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'home.html',
+    controller: 'homeCtrl'
   });
-  
-  
-  app.controller('AboutController', function($scope) {
-    $scope.message = 'Hello from AboutController';
+
+  $routeProvider.when('/HallGov', {
+    templateUrl: 'HallGov.html',
+    controller: 'HallGovCtrl'
   });
+
+  $routeProvider.when('/About', {
+    templateUrl: 'About.html',
+    controller: 'AboutCtrl'
+  });
+  $routeProvider.otherwise({redirecTo: '/'});
+}]);
+
+
+app.controller('homeCtrl', ['$scope', function($scope){
+    $scope.message = 'Welcome to Inspire';
+}]);
+
+app.controller('HallGovCtrl', ['$scope', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+}]);
+
+app.controller('AboutCtrl', ['$scope', function($scope) {
+    $scope.message = 'These are clients';
+}]);

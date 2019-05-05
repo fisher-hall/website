1370458d587837487cff8e6733d29a75f5299655
Dane Williams <dwilli36@nd.edu>
2019-05-05 14:48:52 -0400

    added content to pages

--- app.js
+++ app.js
@@ -8,27 +8,82 @@ app.config(['$routeProvider', function($routeProvider) {
     controller: 'homeCtrl'
   });
 
+  $routeProvider.when('/HallStaff', {
+    templateUrl: 'HallStaff.html',
+    controller: 'HallStaffCtrl'
+  });
+
   $routeProvider.when('/HallGov', {
     templateUrl: 'HallGov.html',
     controller: 'HallGovCtrl'
+  });
+
+  $routeProvider.when('/FisherRegatta', {
+    templateUrl: 'FisherRegatta.html',
+    controller: 'FisherRegattaCtrl'
+  });
+
+  $routeProvider.when('/FisherFunk', {
+    templateUrl: 'FisherFunk.html',
+    controller: 'FisherFunkCtrl'
+  });
+
+  $routeProvider.when('/FredTalks', {
+    templateUrl: 'FredTalks.html',
+    controller: 'FredTalksCtrl'
   });
 
   $routeProvider.when('/About', {
     templateUrl: 'About.html',
     controller: 'AboutCtrl'
   });
+
+  $routeProvider.when('/Alumni', {
+    templateUrl: 'Alumni.html',
+    controller: 'AlumniCtrl'
+  });
+
+  $routeProvider.when('/WelcomeWeekend', {
+    templateUrl: 'WelcomeWeekend.html',
+    controller: 'WelcomeWeekendCtrl'
+  });
+
   $routeProvider.otherwise({redirecTo: '/'});
 }]);
 
 
 app.controller('homeCtrl', ['$scope', function($scope){
-    $scope.message = 'Welcome to Inspire';
+    $scope.message = '';
+}]);
+
+app.controller('HallStaffCtrl', ['$scope', function($scope){
+    $scope.message = '';
 }]);
 
 app.controller('HallGovCtrl', ['$scope', function($scope) {
-    $scope.message = 'Everyone come and see how good I look!';
+    $scope.message = '';
+}]);
+
+app.controller('FisherRegattaCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
+
+app.controller('FisherFunkCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
+
+app.controller('FredTalksCtrl', ['$scope', function($scope){
+    $scope.message = '';
 }]);
 
 app.controller('AboutCtrl', ['$scope', function($scope) {
-    $scope.message = 'These are clients';
+    $scope.message = '';
+}]);
+
+app.controller('AlumniCtrl', ['$scope', function($scope){
+    $scope.message = '';
+}]);
+
+app.controller('WelcomeWeekendCtrl', ['$scope', function($scope){
+    $scope.message = '';
 }]);

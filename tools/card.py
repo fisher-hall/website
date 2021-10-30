4a5436c4904c38bf8b8b9779637dd2caec5c2124
John Sullivan <jpsullivan2019@gmail.com>
2021-10-30 18:16:44 +0100

    adds example of the HTML we need to be outputting

--- tools/card.py
+++ tools/card.py
@@ -3,6 +3,26 @@
 # generate the html for site cards from user input
 # doing it by hand is a pain
 
+"""
+Relevant HTML looks like this:
+
+<div class="col-sm">
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="media/headshots/johnnycarr.jpg" alt="Card image cap" height="275">
+    <div class="card-body">
+        <h5 class="card-title">Fred Fisher</h5>
+    </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Fisher Founder</li>
+            <li class="list-group-item">Major: Sainthood</li>
+            <li class="list-group-item">Hometown: Notre Dame, IN</li>
+            <li class="list-group-item">Hobbies/Interests: building Fisher Hall with my bare hands</li>
+        </ul>
+    </div>
+</div>
+
+"""
+
 def main():
     pass
 

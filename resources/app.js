c0e81ec2b46da2e4316e1eb20738cb61a3f85b34
Daniel Blittschau <d@dblitt.com>
2024-04-07 12:36:15 -0400

    Fix regatta 2024 date

--- resources/app.js
+++ resources/app.js
@@ -100,7 +100,7 @@ app.controller('StAdalbertsCtrl', ['$scope', function($scope){
 
 // Countdown
 // to whoever the new maintainer is - put the date for the next Regatta in here
-var countDownDate = new Date(2024, 4, 13, 10).getTime(); // 2024-04-13 10:00:00
+var countDownDate = new Date(2024, 3, 13, 10).getTime(); // 2024-04-13 10:00:00 (monthIndex is zero indexed)
 
 // Update the count down every 1 second
 var x = setInterval(function() {
